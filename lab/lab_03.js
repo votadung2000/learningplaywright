0728165aed463d69b2c234e2dd8a5dd5d79d7a5e
Nguyen Ngoc Anh (BD/SWD-QTE9) <ang8hc@bosch.com>
2024-07-21 21:59:24 +0700

    update lab3

--- lab/lab_03.js
+++ lab/lab_03.js
@@ -8,19 +8,19 @@ Even numders: 2
 Odd numbers: 3
 ```
 */
-let intArr1 = [1, 2, 3, 4, 5];
-let oddNum = 0;
-let evenNum = 0; 
-for (let index = 0; index < intArr1.length; index++) {
-  if (intArr1[index] % 2 == 0) {
-    evenNum++;
-  } else {
-    oddNum++;
-  }
-}
+// let intArr1 = [1, 2, 3, 4, 5];
+// let oddNum = 0;
+// let evenNum = 0; 
+// for (let index = 0; index < intArr1.length; index++) {
+//   if (intArr1[index] % 2 == 0) {
+//     evenNum++;
+//   } else {
+//     oddNum++;
+//   }
+// }
 
-console.log(`Even numbers: ${evenNum}`); 
-console.log(`Odd numbers: ${oddNum}`);
+// console.log(`Even numbers: ${evenNum}`); 
+// console.log(`Odd numbers: ${oddNum}`);
 
 /** Lab 3.2:
 Finding maximum value/minimum value from an integer array
@@ -33,20 +33,25 @@ Maximum: 5
 ```
 */
 
-let intArr2 = [1, 2, 3, 4, 5];
-let maxNum = intArr2[1];
-let minNum = intArr2[1];
+// let intArr2 = [1, 2, 3, 4, 5];
+// let maxNum = intArr2[0];
+// let minNum = intArr2[0];
 
-for (let index = 0; index < intArr2.length; index++) {
-  if (intArr2[index] >= maxNum) {
-    maxNum = intArr2[index];
-  } else {
-    minNum = intArr2[index];
-  }
-}
+// for (let index = 0; index < intArr2.length; index++) {
+//   if (intArr2[index] >= maxNum) {
+//     maxNum = intArr2[index];
+//   } 
+// }  
 
-console.log(`Max number is: ${maxNum}`); 
-console.log(`Min number is: ${minNum}`);
+// console.log(`Max number is: ${maxNum}`);
+
+// for (let index = 0; index < intArr2.length; index++) {
+//   if (intArr2[index] <= minNum) {
+//     minNum = intArr2[index];
+//   }
+// }
+
+// console.log(`Min number is: ${minNum}`);
 
 /** Lab 3.3:
 Sort an integer array from min to max
@@ -59,7 +64,15 @@ Expected output: [1, 12, 16, 28, 34]
 let intArr3 = [12, 34, 1, 16, 28];
 console.log(intArr3.sort());
 
+// let minNumArr3 = intArr3[0]
 
+// for (let index = 0; index < intArr3.length; index++) {
+//   if (intArr3[index] <= minNumArr3) {
+//     let 
+//     } 
+// }
+
+// console.log(intArr3)
 /** Lab 3.4(optional):
 Merge 2 SORTED integer array into one SORTED array
 
@@ -71,6 +84,6 @@ Expected output: [1, 1, 12, 13, 16, 16, 27, 28, 34, 99]
 ``` 
 */
 
-let Array01 = [1, 12, 16, 28, 34];
-let Array02 = [1, 13, 16, 27, 99];
-console.log(Array01.concat(Array02).sort());
+// let Array01 = [1, 12, 16, 28, 34];
+// let Array02 = [1, 13, 16, 27, 99];
+// console.log(Array01.concat(Array02).sort());
